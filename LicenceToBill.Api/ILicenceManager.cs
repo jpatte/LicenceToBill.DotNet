1d58d0ae5c27ac014a92b981e05782ee4b721ffa
jpatte <julien.patte@gmail.com>
2013-04-18 22:02:08 +0200

    added ILicenseManager interface

---
+++ LicenceToBill.Api/ILicenceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LicenceToBill.Api
+{
+    public interface ILicenceManager
+    {
+        List<UserV2> ListUsers();
+        UserV2 GetUser(string keyUser);
+        UserV2 PostUser(string keyUser, string nameUser, int? lcid = null);
+
+        List<UserV2> ListUsersByFeature(string keyFeature);
+        List<FeatureV2> ListFeatures(int? lcid = null);
+        List<FeatureV2> ListFeaturesByUser(string keyUser);
+        FeatureV2 GetLimitation(string keyFeature, string keyUser);
+
+        List<OfferV2> ListOffers(int? lcid = null);
+        List<OfferV2> ListOffersByUser(string keyUser);
+        
+        List<DealV2> ListDealsByUser(string keyUser);
+        
+        List<FeatureV2> PostTrial(string keyOffer, string keyUser, string nameUser, int? lcid = null);
+
+        void ActivateOffer(string keyUser, string keyOffer);
+        void TerminateOffer(string keyUser, string keyOffer);
+    }
+}
---
+++ LicenceToBill.Api/OptimisticLicenceManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LicenceToBill.Api
+{
+    public class OptimisticLicenceManager : ILicenceManager
+    {
+        public List<UserV2> ListUsers()
+        {
+            return LicenceManager.ListUsers();
+        }
+
+        public UserV2 GetUser(string keyUser)
+        {
+            return LicenceManager.GetUser(keyUser);
+        }
+
+        public UserV2 PostUser(string keyUser, string nameUser, int? lcid = null)
+        {
+            return LicenceManager.PostUser(keyUser, nameUser, lcid);
+        }
+
+        public List<UserV2> ListUsersByFeature(string keyFeature)
+        {
+            return LicenceManager.ListUsersByFeature(keyFeature);
+        }
+
+        public List<FeatureV2> ListFeatures(int? lcid = null)
+        {
+            return LicenceManager.ListFeatures(lcid);
+        }
+
+        public List<FeatureV2> ListFeaturesByUser(string keyUser)
+        {
+            return LicenceManager.ListFeaturesByUser(keyUser);
+        }
+
+        public FeatureV2 GetLimitation(string keyFeature, string keyUser)
+        {
+            return LicenceManager.GetLimitation(keyFeature, keyUser);
+        }
+
+        public List<OfferV2> ListOffers(int? lcid = null)
+        {
+            return LicenceManager.ListOffers(lcid);
+        }
+
+        public List<OfferV2> ListOffersByUser(string keyUser)
+        {
+            return LicenceManager.ListOffersByUser(keyUser);
+        }
+
+        public List<DealV2> ListDealsByUser(string keyUser)
+        {
+            return LicenceManager.ListDealsByUser(keyUser);
+        }
+
+        public List<FeatureV2> PostTrial(string keyOffer, string keyUser, string nameUser, int? lcid = null)
+        {
+            return LicenceManager.PostTrial(keyOffer, keyUser, nameUser, lcid);
+        }
+
+        public void ActivateOffer(string keyUser, string keyOffer)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void TerminateOffer(string keyUser, string keyOffer)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
